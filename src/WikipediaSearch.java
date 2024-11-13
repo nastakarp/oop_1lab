5cc7faec42896a1d5ded0c1b92947d26341543de
Анастасия Карпенко <152171011+nastakarp@users.noreply.github.com>
2024-11-14 00:35:54 +0300

    WikipediaSearch

--- src/WikipediaSearch.java
+++ src/WikipediaSearch.java
@@ -4,7 +4,6 @@ import java.net.*;
 import java.util.Scanner;
 import java.awt.Desktop;
 import java.net.URI;
-
 public class WikipediaSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
